e890aee73dce931af874a9e11d2b21cd926d2c3a
Ahmed Osman <ahmed.osman@kcl.ac.uk>
2022-12-08 14:57:37 +0000

    Solution

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,24 @@
 """Forms of the project."""
+from django import forms
+from django.core.validators import RegexValidator
+from .models import Thing
 
-# Create your forms here.
+class ThingForm(forms.ModelForm):
+    class meta:
+        model = Thing
+        fields = ['name','description', 'quantity','created_at']
+
+    
+    def clean(self):
+        """Clean the data and generate messages for any errors."""
+        super().clean()
+    
+    def save(self):
+        super().save(commit=False)
+        thing = Thing.objects.create(
+            name = self.cleaned_data.get('name'),
+            description = self.cleaned_data.get('descripquantity'),
+            quantity = self.cleaned_data.get('quantity'),
+            created_at = self.cleaned_data.get('created_at')
+        )
+        return thing
--- things/views.py
+++ things/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from forms import ThingForm
+from .forms import ThingForm
 
 def home(request):
     return render(request, 'home.html', {'form': ThingForm})
